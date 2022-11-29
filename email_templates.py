f65c91d1f99474f6419f5434fcc996c1c0ac95f4
vendhunt <118826157+vendhunt@users.noreply.github.com>
2022-11-29 18:59:35 -0500

    Create email_templates.py

---
+++ email_templates.py
@@ -0,0 +1,13 @@
+def main_template(addy,email_id, email, city):
+	html = f'''
+	<div id=":uo" class="a3s aiL "><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr"><div dir="ltr">Hey,&nbsp;<div><br></div><div>I just wanted to reach out as another&nbsp;small business in {city}.&nbsp;</div><div><br></div><div>We are a local vending machine business. We provide, stock and service vending machines at no cost to the business.&nbsp;</div><div><br></div><div>Do you currently have a vending machine provider?&nbsp;</div><div><br></div><div>Thanks,&nbsp;</div><div>Alex</div><div><br></div><div><font size="1">Sent from Vending Solutions<br></font></div></div><font size="1">{addy}</font></div><div dir="ltr"><span style="font-size:x-small">To</span><a href="">unsubscribe</a><span style="font-size:x-small"> reply to this email.</span></div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	</div>
+	<img src="https://mailtrack.vendhunt.com/image/z4Ad3RdNk44R6VzUwXhy.png?cstm_data={email_id}" class="CToWUd" data-bit="iit"></div>
+	'''
+	return html
